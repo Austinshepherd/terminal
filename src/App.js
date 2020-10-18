419d3f8da6d9409867ce28bea0da7d89dc7e9183
Austinshepherd <austink.shepherd@gmail.com>
2020-10-17 17:41:24 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Terminal from 'terminal-in-react';
+import "../src/App.css"
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  showMsg = () => 'Awesome! You were able to complete the first step, now type "next-task"'
+  finishMsg = () => 'Amazing! You are all complete, that was the best coding ive ever seen.'
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%"
+        }}
+      >
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            'open-google': () => window.open('https://www.google.com/', '_blank'),
+            start: this.showMsg,
+            'next-task': this.finishMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            start: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='start coding with "start"'
+        className="Term"/>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
