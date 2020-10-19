a2c53fded7f8aec4d102c2b17405817a24b71ad1
Austinshepherd <austink.shepherd@gmail.com>
2020-10-19 00:08:02 -0700

    filesystem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Terminal from 'terminal-in-react';
 import "../src/App.css"
- 
+import pseudoFileSystemPlugin from 'terminal-in-react-pseudo-file-system-plugin';
+
+
+const FileSystemPlugin = pseudoFileSystemPlugin();
 
 class App extends Component {
 
@@ -10,7 +13,7 @@ class App extends Component {
     return (<div style={{color: "#247ddb" }}>Documents <br/><br/>Media<br/><br/>Projects</div>);
   }
   finishMsg() {
-    return (<div style={{color: "#247ddb" }}>Documents <br/><br/>Media<br/><br/>Projects</div>);
+    return (<div style={{color: "green" }}>Changed Directory!</div>);
   }
  
   
@@ -26,6 +29,9 @@ class App extends Component {
         }}
       >
         <Terminal
+           plugins={[
+            (FileSystemPlugin)
+           ]}
           color='green'
           backgroundColor='black'
           barColor='black'
@@ -43,7 +49,7 @@ class App extends Component {
             ls: 'shows a message',
             alert: 'alert', popup: 'alert'
           }}
-          msg='Dear soon to be hacker prodijy, Welcome to your first lesson in navigating your linux operating system. Lets begin with something basic, the "ls" command. Typing "ls" in the terminal will list all files within your current directory. Try it out for yourself!      
+          msg='Dear (soon to be) hacker prodijy, Welcome to your first lesson in navigating your linux operating system. Lets begin with something basic, the "ls" command. Typing "ls" in the terminal will list all files within your current directory. Try it out for yourself!      
           .....type "help" for a list of all commands in this terminal.'
         className="Term"/>
       </div>
